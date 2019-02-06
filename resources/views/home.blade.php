5584975eddea974979730d1c1c83a963dc09b7a4
J. Alexander Curtis <jalexandercurtis@gmail.com>
2019-02-06 16:32:10 -0700

    Added zondicons and zondicon font. Also completed sidebard navigation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container m-t-10">
-    <div class="sq-row">
-        <div class="sq-col-2">
-            Sidebar
-        </div>
-        <div class="sq-col-10">
-            <div class="sq-card">
-                <div class="sq-card-header">Dashboard</div>
-
-                <div class="sq-card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+<main class="main m-t-25">
+    <div class="container m-t-10">
+        <div class="sq-row">
+            <aside id="sq-main-sidebar" class="sq-col-2">
+                <ul class="sq-sidebar-nav">
+                    <li class="sq-nav-item">
+                        <a href="" class="sq-sidebar-nav-link active">
+                            <i class="zi-dashboard"></i>
+                            Dashboard
+                        </a>
+                    </li>
+                    <li class="sq-nav-item">
+                        <a href="" class="sq-sidebar-nav-link">
+                            <i class="zi-link"></i>
+                            Squished Links
+                        </a>
+                    </li>
+                    <li class="sq-nav-item">
+                        <a href="" class="sq-sidebar-nav-link">
+                            <i class="zi-paste"></i>
+                            Reports
+                        </a>
+                    </li>
+                    <li class="sq-nav-item">
+                        <a href="" class="sq-sidebar-nav-link">
+                            <i class="zi-servers"></i>
+                            Domains
+                        </a>
+                    </li>
+                    <li class="sq-nav-item">
+                        <a href="" class="sq-sidebar-nav-link">
+                            <i class="zi-cog"></i>
+                            Settings
+                        </a>
+                    </li>
+                </ul>
+            </aside>
+            <div class="sq-col-10">
+                <div class="sq-card">
+                    <div class="sq-card-header">Dashboard</div>
+    
+                    <div class="sq-card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+    
+                        You are logged in!
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</main>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
 
   <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/icons.css') }}" rel="stylesheet">
 </head>
 
 <body>
